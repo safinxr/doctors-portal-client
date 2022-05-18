588eb33e77248b11c3c941309dc7fc478915ad13
Safinxr <safinxr@gmail.com>
2022-05-19 01:27:15 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Pages/Navbar/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home/Home';
+import About from './Pages/About/About';
+import Login from './Pages/Authentication/Login/Login';
 
 function App() {
   return (
     <div >
-     <button class="btn btn-primary">Button</button>
+     <Navbar></Navbar>
+     <Routes>
+       <Route path='/home' element={<Home></Home>}></Route>
+       <Route path='/' element={<Home></Home>}></Route>
+       <Route path='/about' element={<About></About>}></Route>
+       <Route path='/login' element={<Login></Login>}></Route>
+     </Routes>
     </div>
   );
 }
